01a02267754f26551b35138cdb95b077019ba36c
Zhiganov Dmitriy <d.zhiganov@qiwi.com>
2020-02-07 10:43:47 +0300

    module2-task1

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import Main from '../main/main.jsx';
 
-// eslint-disable-next-line react/prop-types
-const App = ({promoData}) => {
-  return <Main promoData={promoData}/>;
-};
+const App = (props) => <Main {...props}/>;
 
 export default App;
